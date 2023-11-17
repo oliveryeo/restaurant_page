5e101009e9e0b6f86be4fbcf0ebf662c1b560f2a
oliveryjy <yjyoliver@hotmail.com>
2023-11-17 09:28:47 +0800

    create eventlisteners for header buttons

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import initializePage from './initializePage';
 import loadHomeTab from './homePage';
-// import loadMenuTab from './menuPage';
+import loadMenuTab from './menuPage';
 // import loadContactTab from './contactPage';
 
 console.log("Hello!");
@@ -8,6 +8,17 @@ console.log("Hello!");
 // Add header and footer, and load up homeTab
 initializePage();
 
+const homeButton = document.querySelector('header > button:first-child');
+const menuButton = document.querySelector('header > button:nth-child(2)');
+const contactButton = document.querySelector('header > button:last-child');
+
+homeButton.addEventListener('click', () => {
+  loadHomeTab();
+});
+
+menuButton.addEventListener('click', () => {
+  loadMenuTab();
+});
 
 
 
--- src/menuPage.js
+++ src/menuPage.js
@@ -2,3 +2,22 @@ import './styles.css';
 import wipePage from './wipePage';
 
 // Function to create the Menu view page
+const loadMenuTab = () => {
+  wipePage();
+  // Create title, testimony, hours, location then append to contentBox, then append to #content
+  const title = document.createElement('div');
+  title.classList.add('title');
+  const pigIcon_1 = new Image();
+  pigIcon_1.src = pig_1;
+  const pigIcon_2 = new Image();
+  pigIcon_2.src = pig_2;
+  title.appendChild(pigIcon_1);
+  title.innerHTML += "Menu";
+  title.appendChild(pigIcon_2);
+
+  const menuTitle = document.createElement('div');
+  menuTitle.classList.add('menu-title');
+
+}
+
+export default loadMenuTab;
